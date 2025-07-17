890ae41604ec80e464c30a4a51bc80ccaacec01f
sanketsalunke1001 <sanketsalunke1001@gmail.com>
2025-07-17 20:13:27 +0530

    My second day homework

---
+++ Python Day 1/Day 2/Day 2.py
@@ -0,0 +1,90 @@
+# Q: Assign your name and age to two variables and print them.
+# Example output: My name is Ashutosh and I am 25 years old.
+# S = "Sanket"
+# Age = 26
+
+# print(f"My name is {S} and I am {Age} years old.");
+
+# Q: Identify which variable names are valid or invalid:
+#_myvar = 'Sanket'
+# my_var = 'Sanket'
+# 2ndname = 'Sanket'   # invalid can't start with number
+# user-name = 'Sanket' # invalid can't use - in variable
+#class = 'Sanket'  # invalid can't use due to reserved keywords
+# userName = 'Sanket'
+
+# print(userName); 
+
+# Try writing code with these names and see which give errors.
+# Q: Assign values 10, 20, 30 to a, b, c in one line and print them.
+
+# a, b, c = 10, 20 , 30
+
+# print(a, b, c)
+
+
+# Q: Assign the value 100 to variables x, y, z in one line. Then print all three.
+
+# x = y = z = 100
+
+# print(x, y, z)
+
+# Q: Assign a float to a variable and print its type using type() function.
+
+# number = 5.5
+# print(number)
+# print(type(number))
+
+# Q: Take two variables a = 5, b = 10 and swap them without using a third variable.
+
+# a = 5
+# b = 10
+# a, b = b,a # swaping values using tuple unpacking
+# print("a = ", a)
+# print("b = ", b)
+
+# Q: Use f-string to print: "Ashutosh is 27 years old and lives in Pune"
+# Use variables for name, age, city.
+
+# name = "Sanket"
+# age = 27
+# city = "Mumbai"
+
+# print(f"{name} is {age} years old and lives in {city}")
+
+# Q: Take name and age as input from the user and print: "Hello, [name]! You are [age] years old."
+
+# name = input ("Enter your name: ")
+# age = input("Enter your age: ")
+
+# print(f"Hello, {name}! You are {age} years old.")
+
+# name = "Sanket"
+# age = 26
+
+# print(f"Hello, {name}! you are {age} years old")
+
+# Q: Write a program to take 2 numbers from the user and print their sum, difference, product, and division.
+
+# number1 = float(input("Enter the first number: "))
+# number2 = float(input("Enter your second number: "))
+
+# sum = number1 + number2
+# difference = number1 - number2
+# product = number1 * number2
+# division = number1 / number2
+
+# print(f"sum = {sum}")
+# print(f"difference ={difference}")
+# print(f"product = {product}")
+# print(f"division ={division}")
+
+# Q: Define a variable `PI = 3.14`. Use it to calculate the area of a circle with radius 5.
+# Note: Though Python doesn't support constants, use capital letters as a convention.
+
+# PI = 3.14
+# radius = 5 
+
+# area = radius * PI **2
+
+# print(f"Area of the circle with Radius {radius} is Area {area} ")
